361bcec51c19de7e5c45f17215072275f9f27788
a-2280 <khaliff.williamss@gmail.com>
2025-10-04 15:31:52 -0600

    removed overflow

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -3,7 +3,7 @@ import Footer from "@/components/Footer";
 
 export default function MainLayout({ children }) {
   return (
-    <div className="h-screen w-screen flex flex-col justify-between px-[25px] overflow-x-hidden overflow-y-hidden">
+    <div className="h-screen w-screen flex flex-col justify-between px-[25px] overflow-x-hidden">
       <Navbar />
       {children}
       <Footer />
