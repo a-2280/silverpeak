16108a4c9b343cc7dcec622c8ae6ce5f557fd3e7
a-2280 <khaliff.williamss@gmail.com>
2025-10-07 22:22:41 -0600

    pre loaded fonts and added more space at the bottom of the about page

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preload" href="/fonts/Figtree/Figtree-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/Figtree/Figtree-SemiBold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/IBM/IBMPlexMono-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+      </head>
       <body>{children}</body>
     </html>
   );
