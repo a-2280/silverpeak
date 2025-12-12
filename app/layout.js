a77e712580aaaf77eb69b54dec16cfac7512e252
Calvin <calvin@Mac.lan>
2025-12-11 18:11:53 -0800

    removed icons from metadata

--- app/layout.js
+++ app/layout.js
@@ -3,9 +3,6 @@ import "./globals.css";
 export const metadata = {
   title: "Silverpeak",
   description: "Engineering",
-  icons: {
-    icon: '/icon.ico',
-  },
 };
 
 export default function RootLayout({ children }) {
