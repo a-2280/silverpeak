1c48070caa60a742b706233033a71715618e8737
Calvin <calvin@Mac.lan>
2026-01-09 14:19:23 -0800

    fixed footer text size

--- components/Footer.js
+++ components/Footer.js
@@ -15,22 +15,22 @@ export default function Footer() {
     <>
       <div className="w-full fixed bottom-0 left-0 flex justify-between items-center py-[19px] px-[25px] bg-[#F0F0F0] flex-wrap z-10 lg:bg-transparent lg:flex-row lg:items-end">
         <div className="min-[405px]:w-full min-[405px]:flex min-[405px]:justify-between min-[405px]:items-baseline lg:flex-col lg:gap-[9px] lg:w-1/2">
-          <p className="text-nowrap !text-[10px] pb-[6px] lg:pb-0">
+          <p className="text-nowrap !text-[10px] lg:!text-[13px] pb-[6px] lg:pb-0">
             +1 (801) 499 5045{" / "}
             <button
-              className="!no-underline !text-[10px] hover:!text-blue"
+              className="!no-underline !text-[10px] lg:!text-[13px] hover:!text-blue"
               onClick={() => setOpen(!open)}
             >
               EMAIL
             </button>
           </p>
-          <p className="text-end w-fit text-nowrap !text-[10px]">
+          <p className="text-end w-fit text-nowrap !text-[10px] lg:!text-[13px]">
             &copy;{currentYear} SILVERPEAK ENGINEERING
           </p>
         </div>
         {isAboutPage && (
           <div className="hidden lg:flex lg:w-1/2 justify-start items-end">
-            <p className="text-end w-fit text-nowrap !text-[10px]">
+            <p className="text-end w-fit text-nowrap !text-[10px] lg:!text-[13px]">
               177 E. Antelope Drive Suite B, Layton, UT 84041
             </p>
           </div>
